d789bc64fc6e3a088c1e020cc0ab021de3485410
Alex Pott <alex.a.pott@googlemail.com>
2017-03-16 13:08:17 +0000

    Use .platform.app.yaml for service determination

--- src/Command/Docker/RebuildCommand.php
+++ src/Command/Docker/RebuildCommand.php
@@ -13,7 +13,6 @@ use mglaman\Docker\Docker;
 use mglaman\PlatformDocker\Config;
 use mglaman\PlatformDocker\Platform;
 use mglaman\PlatformDocker\PlatformAppConfig;
-use mglaman\PlatformDocker\PlatformServiceConfig;
 use mglaman\PlatformDocker\Stacks\StacksFactory;
 use mglaman\Toolstack\Toolstack;
 use mglaman\Toolstack\Stacks;
@@ -78,10 +77,10 @@ class RebuildCommand extends DockerCommand
         }
         // Support services defined in .platform/services.yaml
         else {
-            if (PlatformServiceConfig::hasRedis()) {
+            if (PlatformAppConfig::hasRedis()) {
                 $composeContainers->addRedis();
             }
-            if (PlatformServiceConfig::hasSolr()) {
+            if (PlatformAppConfig::hasSolr()) {
                 $composeContainers->addSolr();
             }
         }
