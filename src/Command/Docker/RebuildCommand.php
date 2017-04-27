f854a18bd66bcaed88a107b5d1b93bf78e998a14
Alex Pott <alex.a.pott@googlemail.com>
2017-04-27 10:58:25 +0100

    Remove volumes too

--- src/Command/Docker/RebuildCommand.php
+++ src/Command/Docker/RebuildCommand.php
@@ -94,8 +94,7 @@ class RebuildCommand extends DockerCommand
         }
 
         // Stop and remove any existing containers.
-        Compose::stop();
-        Compose::rm(TRUE, TRUE);
+        Compose::down(TRUE);
 
         $this->stdOut->writeln('<info>Building the containers</info>');
         Compose::build();
